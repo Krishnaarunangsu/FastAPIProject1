dda7afdc431b4ef206c67be743b43be35cb73985
Arunangsu Sahu <arunangsutech@gmail.com>
2022-11-23 12:27:36 +0530

    Commit the changes in the main.py. Added more operations

--- main.py
+++ main.py
@@ -5,6 +5,39 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """
+    root function with concurrency
+    :return:
+    """
     return {
-        "message": "Hello World!"
+        "message": "Hello Krishna!"
     }
+
+
+@app.get("/normal")
+def root_normal():
+    """
+    Normal root function
+    :return:
+    """
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id:int}")
+async def read_item(item_id):
+    """
+    Read the item id
+    :param item_id:
+    :return:
+    """
+    return {"item_id": item_id}
+
+
+@app.get("/cost/{individual_cost:float}")
+async def read_individual_cost(individual_cost):
+    """
+    Read the individual cost
+    :param individual_cost:
+    :return:
+    """
+    return {"individual_cost": individual_cost}
